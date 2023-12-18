cb4eb8c92cd9800ba8f6d5298ae23a16037af34f
bubblecocoa <bubblecocoa@gmail.com>
2023-12-18 20:22:24 +0900

    feat: NetworkProvider 생성
    해당 클래스를 이용해 TVNetwork, MovieNetwork를 생성할 수 있음

---
+++ TVMovieRanking/Network/NetworkProvider.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkProvider.swift
+//  TVMovieRanking
+//
+//  Created by 정동원 on 12/18/23.
+//
+
+// 어떤 네트워크든지 생성할 수 있는 클래스
+final class NetworkProvider {
+    private let endpoint: String
+    
+    init() {
+        self.endpoint = "https://api.themoviedb.org/3"
+    }
+    
+    func makeTVNetwork() -> TVNetwork {
+        let network = Network<TVListModel>(endpoint)
+        
+        return TVNetwork(network: network)
+    }
+    
+    func makeMovieNetwork() -> MovieNetwork {
+        let network = Network<MovieListModel>(endpoint)
+        
+        return MovieNetwork(network: network)
+    }
+}
