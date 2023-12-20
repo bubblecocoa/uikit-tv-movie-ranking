e8526f583c43329888ed2423254fae322ea23571
bubblecocoa <bubblecocoa@gmail.com>
2023-12-20 20:25:30 +0900

    feat: 배너 타입 셀 구현

--- TVMovieRanking/ViewController.swift
+++ TVMovieRanking/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController {
     lazy var collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout())
         collectionView.register(NormalCollectionViewCell.self, forCellWithReuseIdentifier: NormalCollectionViewCell.id)
+        collectionView.register(BigImageCollectionViewCell.self, forCellWithReuseIdentifier: BigImageCollectionViewCell.id)
         
         return collectionView
     }()
